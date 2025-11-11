3a5c350acf3709c69ae51b9752f1b5ee9a4a2b59
Ahmad <ahmad.pakmehr@gmail.com>
2025-11-11 22:50:53 +0100

    a note about dog age calculating using formula added

--- js_week1.js
+++ js_week1.js
@@ -1,20 +1,22 @@
-
+// Assignment 1
 console.log("Assignment 1:");
 const yearOfBirth = 1983; 
 let yearFuture = 2045;  
 let age = yearFuture - yearOfBirth;
 console.log("You will be " + age + " years old in " + yearFuture);
 
+// Assignment 2
 console.log("Assignment 2:");
 const dogYearOfBirth = 2018;
 let dogYearFuture = 2030;
 let dogYear = dogYearFuture - dogYearOfBirth;
 let shouldShowResultInDogYears = true;
+// Calculating dog age according to human years, using the formula I found on net!
 let dogAge= Math.round(16 * Math.log(dogYear+1) + 31);
-
 console.log("Your dog will be " + dogYear + " human years old in " + dogYearFuture + 
             ". Or your dog will be " + dogAge + " dog years old in " + dogYearFuture + ".");
-
+        
+// Assignment 3
 console.log("Assignment 3:");
 const PeterHouseWidth = 8;
 const PeterHouseDepth = 10;
@@ -25,6 +27,7 @@ const JuliaHouseWidth = 5;
 const JuliaHouseDepth = 11;
 const JuliaHouseHeight = 8;
 const JuliaGardenSizeInM2 = 70;
+
 let PeterHouseVolume = PeterHouseWidth * PeterHouseDepth * PeterHouseHeight;
 let JuliaHouseVolume = JuliaHouseWidth * JuliaHouseDepth * JuliaHouseHeight;
 let PeterHousePrice = PeterHouseVolume * 2.5 * 1000 + PeterGardenSizeInM2 * 300;
@@ -41,11 +44,10 @@ if  (JuliaHousePrice>1000000){
     console.log("Julia has Paid more than the real value for her house.");
 }   
 
+// Assignment 4
 console.log("Assignment 4:");
 const firstWords = ["Easy", "Awesome", "Fantastic", "Incredible", "Amazing", "Impressive", "Astonishing", "Astounding", "Fabulous", "Magnificent"];
 const secondWords = ["StartUp", "Developer", "Project", "Course", "Journey", "Experience", "Knowledge", "Skill", "Future", "Opportunity"];
 let randomFirstWord = firstWords[Math.floor(Math.random()*10)];
 let randomSecondWord = secondWords[Math.floor(Math.random()*10)];
-// console.log("StartUp name: " + randomFirstWord + " " + randomSecondWord);
-
 console.log(`StartUp name: ${randomFirstWord} ${randomSecondWord}`);
