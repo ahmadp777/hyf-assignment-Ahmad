22b22a84318e7398da40232963ba8958d76cf802
Ahmad <ahmad.pakmehr@gmail.com>
2025-12-17 13:19:56 +0100

    Fixed mentioned suggestions

--- DB_Assignment.sql
+++ DB_Assignment.sql
@@ -7,15 +7,12 @@ FROM task
 WHERE due_date IS NULL;
 
 -- 3.Find all the tasks that are marked as done. 
-SELECT * FROM task  
-JOIN status ON task.status_id = status.id 
-WHERE status.name= 'Done';
+SELECT * FROM task
+WHERE status_id IN (SELECT id FROM status WHERE name = 'Done');
 
 -- 4.Find all the tasks that are not marked as done.
-SELECT * FROM task  
-JOIN status ON task.status_id = status.id 
-WHERE status.name <> 'Done';
-
+SELECT * FROM task
+WHERE status_id IN (SELECT id FROM status WHERE name <> 'Done');
 -- 5.Get all the tasks, sorted with the most recently created first.
 SELECT * FROM task ORDER BY created DESC; 
 
@@ -30,20 +27,20 @@ OR LOWER(description)  LIKE "%database%";
 -- 8.Get the title and status (as text) of all tasks.
 SELECT t.title , s.name AS status
 FROM task t
-JOIN status s ON t.status_id = s.id;
+INNER JOIN status s ON t.status_id = s.id;
 
 -- 9.Get the name of each status, along with a count of how many tasks have that status.
 SELECT s.name AS status , COUNT (*) AS totalSameStatusTasks
 FROM status s
-JOIN task t ON t.status_id = s.id 
+INNER JOIN task t ON t.status_id = s.id 
 GROUP BY s.name;
 
 -- 10.Get the names of all statuses, sorted by the status with most tasks first.
 SELECT s.name AS status , COUNT (*) AS totalSameStatusTasks2
 FROM status s
-JOIN task t ON t.status_id = s.id 
+INNER JOIN task t ON t.status_id = s.id 
 GROUP BY s.name
-ORDER BY COUNT() DESC;
+ORDER BY totalSameStatusTasks2 DESC;
 
 
 
